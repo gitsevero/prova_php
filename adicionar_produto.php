9d3abd743a062369c47be7ddf071c0dfa7f1f69e
severo dev <119140931+gitsevero@users.noreply.github.com>
2023-05-26 00:38:31 -0300

    funcional

---
+++ adicionar_produto.php
@@ -0,0 +1,28 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "9jnE[NGWw!zXHLqX";
+$database = "product_crud";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+}
+
+$nome_categoria = $_POST['nome_categoria'];
+$nome_produto = $_POST['nome_produto'];
+$descricao = $_POST['descricao'];
+$preco = $_POST['preco'];
+
+$sql = "INSERT INTO produtos (nome_categoria, nome_produto, descricao, preco) VALUES ('$nome_categoria', '$nome_produto', '$descricao', '$preco')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Produto adicionado com sucesso!";
+} else {
+    echo "Erro ao adicionar o produto: " . $conn->error;
+}
+
+$conn->close();
+?>
